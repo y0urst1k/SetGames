db9427e773e6d02d64869aa689cc9f5c6eddcc77
Sayl1ght <Sayl1ght@gmail.com>
2025-10-19 17:41:30 +0300

    feat: add a novel module

---
+++ SetGames/SetGames/Modules/The Hero's Way/THW.h
@@ -0,0 +1,5 @@
+#pragma once
+void WriteSeparator(int count, char separator = '*');
+void OutputBlock(string &message, string &character = "");
+int AskChoice(const string& question, const vector<string>& options);
+void Episode1();
