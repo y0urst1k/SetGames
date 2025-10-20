d38108d97190b1fb6563403bec6b56b3e423b03b
Sayl1ght <Sayl1ght@gmail.com>
2025-10-20 15:44:46 +0300

    refactor: delete Modules directory

--- SetGamesProject/Modules/TheHerosWay/THW.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-#include <vector>
-
-void WriteSeparator(int count, char separator = '*');
-void OutputBlock(std::string& message, std::string& character);
-int AskChoice(const std::string& question, const std::vector<std::string>& options);
-void Episode1();
